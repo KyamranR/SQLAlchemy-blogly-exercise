d69b981e1bb6e05605cc437163b2ee2b7b390a3b
Kyamran Rizayev <kyamranr@gmail.com>
2024-03-09 12:41:15 -0500

    changed delete route to redirect to user page instead of list of users

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
     db.session.delete(post)
     db.session.commit()
-    return redirect(url_for('list_users'))
+    return redirect(url_for('show_user'))
 
 
 if __name__ == '__main__':
